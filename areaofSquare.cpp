46a547a8ee32a6ff880eec5fea41172527273f30
VishvaRibadiya <166017828+VishvaRibadiya@users.noreply.github.com>
2024-04-06 11:58:39 +0530

    third project

---
+++ areaofSquare.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int l,area;
+
+    cout << " Enter yout lenght :";
+    cin >> l;
+
+    area=l*l;
+    cout << " Your area of square is : " << area << "\n";
+
+    return 0; 
+}
