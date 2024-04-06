6e3926b7eafc46e1bc091f760010af5bbd77aec3
VishvaRibadiya <166017828+VishvaRibadiya@users.noreply.github.com>
2024-04-06 12:07:59 +0530

    square

---
+++ areaofRectangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int l,b,area;
+
+    cout << " Enter value of L:";
+    cin >> l;
+
+    cout << " Enter value of B: ";
+    cin >> b;
+
+    area=l*b;
+    cout << " Your area of Rectangle is :" << area << "\n";
+
+    return 0;
+}
