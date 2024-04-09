0b505f73fe2ca64e164864b35fab11d410acb8a6
VishvaRibadiya <166017828+VishvaRibadiya@users.noreply.github.com>
2024-04-09 15:21:58 +0530

    Inherintance

---
+++ suminMultiInheritance.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class A
+{
+  public:
+  int x;
+  void math()
+  {
+    cout << " Enter the value of X:";
+    cin >> x;
+  }
+};
+class B 
+{
+   public:
+   int y;
+  void calci()
+   {
+     cout << " Enter the value of Y:";
+     cin >> y;
+   }
+};
+class C : public A , public B
+{
+    public:
+    int sum;
+    void sum1()
+    {
+       sum=x+y;
+       cout << "Your ans is:" << sum;
+       
+    }
+
+};
+int main()
+{
+    C myS;
+    myS.math();
+    myS.calci();
+    myS.sum1();
+    return 0;
+
+}
